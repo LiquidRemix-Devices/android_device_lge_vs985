8ca2d037e6b8bbae2ef72bcbd38b0417455b8b33
Aleksander <johnnylittleplanet@gmail.com>
2018-05-06 22:06:09 +0200

    g3: initial liquid g3 [2/2]

--- liquid.mk
+++ liquid.mk
@@ -20,9 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vs985 device
 $(call inherit-product, device/lge/vs985/device.mk)
 
+# Inherit some common LiquidRemix stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := vs985
-PRODUCT_NAME := full_vs985
+PRODUCT_NAME := liquid_vs985
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-VS985
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="lge/g3_vzw/g3:6.0/MRA58K/160141503d43c:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_vzw-user 6.0 MRA58K 160141503d43c release-keys"
