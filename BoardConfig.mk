fad44f1721ac9b9db78fd55541abbc5de6ab21e5
Aleksander <johnnylittleplanet@gmail.com>
2018-06-16 16:24:31 +0200

    Revert "vs985: Update for early-mounted system partition"
    This reverts commit 195990320b730ac788233b1300c07348ab9f591a.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/lge/vs985/rootdir/etc/fstab.recovery
+TARGET_RECOVERY_FSTAB := device/lge/vs985/rootdir/etc/fstab.g3
 
 # RIL
 BOARD_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_10
